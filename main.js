e5a255049a7d055f4e6a3627597816c7b19064a6
leTabs <125308756+leTabs@users.noreply.github.com>
2023-06-12 14:30:27 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,18 +1,16 @@
-const relational = document.querySelector('.relational')
-const python = document.querySelector('.python')
+// ADD COMMENTS DUMMY
 const html = document.querySelector('.html')
 const css = document.querySelector('.css')
+const js = document.querySelector('.js')
+const python = document.querySelector('.python')
+const relational = document.querySelector('.relational')
 
-relational.addEventListener('click', ()=>{
-    window.location = 'sql_main.html'
-})
-python.addEventListener('click', ()=>{
-    window.location = 'python_main.html'
-})
+
 html.addEventListener('click', ()=>{window.location = 'html_main.html'})
 css.addEventListener('click', ()=>{window.location = 'css_main.html'})
-
-
+js.addEventListener('click', ()=>{window.location = 'js_main.html'})
+python.addEventListener('click', ()=>{window.location = 'python_main.html'})
+relational.addEventListener('click', ()=>{window.location = 'sql_main.html'})
 
 
 // 
