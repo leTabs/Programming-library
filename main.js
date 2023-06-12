a4db176c5d0affbd4f39cfab420a1dc34c5a3a99
leTabs <125308756+leTabs@users.noreply.github.com>
2023-06-12 14:20:06 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
+const relational = document.querySelector('.relational')
+const relational_btn = document.querySelector('.relational_btn')
+const python = document.querySelector('.python')
 
+
+relational.addEventListener('click', ()=>{
+    window.location = 'sql_main.html'
+})
+
+python.addEventListener('click', ()=>{
+    window.location = 'python_main.html'
+})
+
+relational_btn.addEventListener('click', ()=>{
+    alert('kiss')
+})
