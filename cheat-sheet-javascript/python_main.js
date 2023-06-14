bcc1bc94fb4665a6597f87dc6cdb2042f9dd486d
leTabs <125308756+leTabs@users.noreply.github.com>
2023-06-14 16:24:50 +0300

    Update python_main.js

--- cheat-sheet-javascript/python_main.js
+++ cheat-sheet-javascript/python_main.js
@@ -2,23 +2,19 @@
 const chapters = document.querySelector('.chapters')
 chapters.innerHTML += `<h3>Introduction</h3>`
 pythonChapters = [
-    'Introduction',
-    'Basics',
     'Syntax',
-    'Data types',
-]
-
-for(item in pythonChapters){
-    chapters.innerHTML +=  `<p class="chapterNames chapter${item}">${pythonChapters[item]}</p>`
-}
-chapters.innerHTML += `<h3>After basics</h3>`
-pythonChapters = [
+    'Data Types',
     'Variables',
     'Operators',
     'Strings',
-    'String Formating',
-    'Basic Build-in Functions',
-    'Controw flow'
+    'Basic Functions'
+]
+for(item in pythonChapters){
+    chapters.innerHTML +=  `<p class="chapterNames chapter${item}">${pythonChapters[item]}</p>`
+}
+chapters.innerHTML += `<h3>Control flow</h3>`
+pythonChapters = [
+    'if Statment'
 ]
 for(item in pythonChapters){
     chapters.innerHTML +=  `<p class="chapterNames">${pythonChapters[item]}</p>`
