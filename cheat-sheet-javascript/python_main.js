f302ab93c06444e831f8a9f214eb84067cd08cf2
leTabs <125308756+leTabs@users.noreply.github.com>
2023-06-14 16:39:33 +0300

    Update python_main.js

--- cheat-sheet-javascript/python_main.js
+++ cheat-sheet-javascript/python_main.js
@@ -1,6 +1,16 @@
 
 const chapters = document.querySelector('.chapters')
-chapters.innerHTML += `<h3>Introduction</h3>`
+const context = document.querySelector('.context')
+context.innerHTML = `
+<h1>Introduction</h1>
+<p>Python is high-level programming language. It’s known for its simple syntax, wide variety of uses and as one of the most beginner friendly languages.
+Python is used for:
+<p>
+<ul>
+    <li>Web</li>
+</ul>
+`
+chapters.innerHTML += `<h3>Introduction</h3><hr>`
 pythonChapters = [
     'Syntax',
     'Data Types',
@@ -12,9 +22,12 @@ pythonChapters = [
 for(item in pythonChapters){
     chapters.innerHTML +=  `<p class="chapterNames chapter${item}">${pythonChapters[item]}</p>`
 }
-chapters.innerHTML += `<h3>Control flow</h3>`
+chapters.innerHTML += `<h3>Control flow</h3><hr>` 
 pythonChapters = [
-    'if Statment'
+    'if Statment',
+    'while loop',
+    'for loop',
+    'range()'
 ]
 for(item in pythonChapters){
     chapters.innerHTML +=  `<p class="chapterNames">${pythonChapters[item]}</p>`
