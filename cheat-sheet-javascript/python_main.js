f74fac25600156d828e7c208bed7040c5ed6f031
leTabs <125308756+leTabs@users.noreply.github.com>
2023-06-15 11:38:56 +0300

    Update python_main.js

--- cheat-sheet-javascript/python_main.js
+++ cheat-sheet-javascript/python_main.js
@@ -1,10 +1,10 @@
 
 const chapters = document.querySelector('.chapters')
 // const context = document.querySelector('.context')
-
+// chapters.textContent += 'pop'
 // The list of each concept etc.
-chapters.innerHTML += `<h1>Introduction</h1><hr>`
-chapters.innerHTML += `<h3>Basics</h3>`
+chapters.innerHTML+= `<h1>Introduction</h1><hr>`
+
 basics = [
     'Syntax',
     'Data Types',
@@ -13,10 +13,11 @@ basics = [
     'Strings',
     'Basic Functions'
 ]
+chapters.innerHTML += `<h3>Basics</h3>`
 for(item in basics){
     chapters.innerHTML +=  `<p class="chapterNames chapter${item}">${basics[item]}</p>`
 }
-chapters.innerHTML += `<h3>Control flow</h3><hr>` 
+
 controlFlow = [
     'if Statments',
     'Ternary Operator',
@@ -25,6 +26,31 @@ controlFlow = [
     'break', 
     'continue'
 ]
+chapters.innerHTML += `<h3>Control flow</h3>` 
 for(item in controlFlow){
     chapters.innerHTML +=  `<p class="chapterNames">${controlFlow[item]}</p>`
+}
+
+
+functios = [
+    'Functions',
+    'return Statment',
+    'lambda Functions',
+    'Scopes'
+]
+chapters.innerHTML += `<h3>Functions</h3>`
+for(item in functios){
+    chapters.innerHTML += `<p class="chapterNames">${functios[item]}</p>`
+}
+
+dataStructures = [
+    'Data Structures',
+    'Lists',
+    'Tuples',
+    'Sets',
+    'Dictionaries'
+]
+chapters.innerHTML += '<h3>Data Structures</h3>'
+for(item in dataStructures){
+    chapters.innerHTML += `<p class="chapterNames">${dataStructures[item]}</p>`
 }
