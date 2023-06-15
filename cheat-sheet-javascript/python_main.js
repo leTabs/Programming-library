18078bec2e4bca450546576e48583c65e74c5cd2
leTabs <125308756+leTabs@users.noreply.github.com>
2023-06-15 13:05:36 +0300

    Update python_main.js

--- cheat-sheet-javascript/python_main.js
+++ cheat-sheet-javascript/python_main.js
@@ -1,68 +1,137 @@
-body{
-    max-width:100vw;
-    max-height: 100vh;
-    overflow: hidden;
-}
-body, body *{
-    padding: 0;
-    margin: 0;
-}
-nav{
-    display: flex;
-    justify-content:flex-start;
-}
-.concept-icon{
-    margin-left: 3rem;
-}
-main{
-    display: grid;
-    grid-template-columns: 0.75fr 3.25fr;
-    overflow: hidden;
-    color: white;
-}
-section{
-    overflow-y: auto;
-    height: calc(100vh - 3.5rem);
-    padding: 1rem;
-    text-align: left;
-}
-h3{
-    color: rgb(255, 255, 255);
-    font-size: 1.5rem;
-    margin:0.5rem;
-    margin-top: 1.5rem;
-    /* text-decoration: underline; */
-}
-.chapterNames{
-    font-size: 1.rem;
-    color: rgb(7, 161, 169);
-    margin: 0.7rem 0 0.5rem 2rem;
-    transition: 300ms;
-}
-.chapterNames:last-child{
-    margin-bottom: 3rem;
-}
-.chapterNames:hover{
-    color: rgb(255, 255, 255);
-    cursor: pointer;
-}
-hr{
-    width: 80%;
+
+const chapters = document.querySelector('.chapters')
+basics = [
+    'Syntax',
+    'Data Types',
+    'Variables',
+    'Operators',
+    'Strings',
+    'Basic Functions'
+]
+controlFlow = [
+    'if Statments',
+    'Ternary Operator',
+    'while Loop',
+    'for Loop',
+    'break', 
+    'continue'
+]
+functios = [
+    'Functions',
+    'return Statment',
+    'lambda Functions',
+    'Scopes'
+]
+dataStructures = [
+    'Data Structures',
+    'Lists',
+    'Tuples',
+    'Sets',
+    'Dictionaries'
+]
+fileDirs = [
+    'Paths / Directories',
+    'Handling Paths',
+    'Working with files',
+]
+debugging = [
+    'Debugging',
+    'raise Statement',
+    'Assertions',
+    'Exception Handling'
+]
+oop = [
+    'OOP',
+    'Classes',
+    'Objects',
+    'Attributes',
+    'Methods',
+    'Inheritence',
+    'Encapsulation',
+    'Polymorphism'
+]
+modules  = [
+    'Modules & Packages',
+    'Working with Modules'
+]
+json_ = [
+    'working with Json'
+]
+dates = [
+    'Dates',
+    'Working with Dates'
+]
+function lists(){
+    chapters.innerHTML+= `<h1>Introduction</h1><hr>`
+    chapters.innerHTML += `<h3>Basics</h3>`
+    for(item in basics){
+        chapters.innerHTML +=  `<p class="chapterNames chapter${item}">${basics[item]}</p>`
+    }
+    chapters.innerHTML += `<h3>Control flow</h3>` 
+    for(item in controlFlow){
+        chapters.innerHTML +=  `<p class="chapterNames">${controlFlow[item]}</p>`
+    }
+    chapters.innerHTML += `<h3>Functions</h3>`
+    for(item in functios){
+        chapters.innerHTML += `<p class="chapterNames">${functios[item]}</p>`
+    }
+    chapters.innerHTML += '<h3>Data Structures</h3>'
+    for(item in dataStructures){
+        chapters.innerHTML += `<p class="chapterNames">${dataStructures[item]}</p>`
+    }
+    chapters.innerHTML += `<h3> Files and Paths</h3>`
+    for(item in fileDirs){
+        chapters.innerHTML += `<p class="chapterNames">${fileDirs[item]}</p>`
+    }
+    chapters.innerHTML += '<h3>Debugging</h3>'
+    for(item in debugging){
+        chapters.innerHTML += `<p class="chapterNames">${debugging[item]}</p>`
+    }
+    chapters.innerHTML += '<h3>Object-Oriented Programming</h3>'
+    for(item in oop){
+        chapters.innerHTML += `<p class="chapterNames">${oop[item]}</p>`
+    }
+    chapters.innerHTML += '<h3>Modules</h3>'
+    for(item in modules){
+        chapters.innerHTML += `<p class="chapterNames">${modules[item]}</p>`
+    }
+    chapters.innerHTML += '<h3>Dates</h3>'
+
+    for(item in dates){
+        chapters.innerHTML += `<p class="chapterNames">${dates[item]}</p>`
+    }
+    chapters.innerHTML += '<h3>Json</h3>'
+    for(item in json_){
+        chapters.innerHTML += `<p class="chapterNames">${json_[item]}</p>`
+    }
 }
 
+lists()
 
-/* ACTUAL CONTEXT */
-.context{
-    border: solid white;
-    padding:4rem
-}
-.title-con{
 
-}
-.description{
-
-}
-.clasic-ul{
-
-}
-/* END ACTUAL CONTEXT */
+const context = document.querySelector('.context')
+context.innerHTML = `
+    <h1 class="title-con">Introduction</h1>
+    <p class="description">Python is high-level programming language. It’s known for its simple syntax,
+        wide variety of uses and as one of the most beginner friendly languages.</p>
+    <hr />
+    <p class="description">Python is used for:</p>
+    <ul class="clasic-ul">
+        <li>Web development</li>
+        <li>Data Analysis and Visualization</li>
+        <li>Machine Learning and AI</li>
+        <li>Command-line applications</li>
+        <li>Game development</li>
+        <li>Networking Programming</li>
+        <li>And more…</li>
+    </ul>
+    <hr />
+    <p class="description">A program created with the python programming language consists of python code written
+        in one or more files. When the files run (compile) they read the code from top to 
+        bottom.
+    </p>
+    <div class="next-prev-btn">
+        <button>Previous</button>
+        <button>Next</button>
+    </div>
+`
