8e79be06578d1768cb840c0f07c15819a0d1c7da
leTabs <125308756+leTabs@users.noreply.github.com>
2023-06-15 11:50:50 +0300

    Update python_main.js

--- cheat-sheet-javascript/python_main.js
+++ cheat-sheet-javascript/python_main.js
@@ -1,8 +1,6 @@
 
 const chapters = document.querySelector('.chapters')
 // const context = document.querySelector('.context')
-// chapters.textContent += 'pop'
-// The list of each concept etc.
 chapters.innerHTML+= `<h1>Introduction</h1><hr>`
 
 basics = [
@@ -30,8 +28,6 @@ chapters.innerHTML += `<h3>Control flow</h3>`
 for(item in controlFlow){
     chapters.innerHTML +=  `<p class="chapterNames">${controlFlow[item]}</p>`
 }
-
-
 functios = [
     'Functions',
     'return Statment',
@@ -53,4 +49,23 @@ dataStructures = [
 chapters.innerHTML += '<h3>Data Structures</h3>'
 for(item in dataStructures){
     chapters.innerHTML += `<p class="chapterNames">${dataStructures[item]}</p>`
+}
+fileDirs = [
+    'Paths / Directories',
+    'Handling Paths',
+    'Working with files',
+]
+chapters.innerHTML += `<h3> Files and Paths</h3>`
+for(item in fileDirs){
+    chapters.innerHTML += `<p class="chapterNames">${fileDirs[item]}</p>`
+}
+debugging = [
+    'Debugging',
+    'raise Statement',
+    'Assertions',
+    'Exception Handling'
+]
+chapters.innerHTML += '<h3>Debugging</h3>'
+for(item in debugging){
+    chapters.innerHTML += `<p class="chapterNames">${debugging[item]}</p>`
 }
