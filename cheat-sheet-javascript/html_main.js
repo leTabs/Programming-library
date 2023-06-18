8a7cd67461b5635c70145c7db0f0b415011b373b
leTabs <125308756+leTabs@users.noreply.github.com>
2023-06-18 17:43:47 +0300

    Update html_main.js

--- cheat-sheet-javascript/html_main.js
+++ cheat-sheet-javascript/html_main.js
@@ -5,7 +5,7 @@ const basics = [
     'Tags',
     'Elements',
     'Page Structure',
-    'Atributes'
+    'Attributes'
 ]
 
 function list(){
@@ -92,4 +92,28 @@ The < head > tag includes information about the page, such as the title. <br />
 The < body > tag nests other elements and tags that compose the content of the webpage.
 
     </p>
+    <p> Note: A good practice is to add a tab-sized indentation to elements and tags that are nested inside of other tags. This helps on understanding the hierarchy of the components, especially as the page grows.</p>
     ` })
+
+
+    const basic3 = document.querySelector('.basic3')
+    basic3.addEventListener('click', ()=>{
+        context.innerHTML = ` 
+    <h1>Attributes</h1>
+    <p>
+        Attributes provide information to an element or modify it (functionality & appearance). An attribute needs to be included in the opening tag of an element. An element can have multiple attributes. Attributes consist of a name and a value (the value is encapsulated to double quotes).
+        <br />
+        Note: not all attributes require a value.
+    </p>
+    <p>Purposes of attributes:</p>
+    <ul>
+        <li>Information: they can provide information witch can be used by HTML or other combinations like Css and JavaScript.</li>
+        <li>Behavior: attributes can define behaviors for elements, such as destination for hyperlinks, alternative text for accessibility purposes etc. </li>
+        <li>Appearance: certain attributes change the appearance of an element or allow for the inclusion of inline Css (Css is a Cascading language with the ability to alter elements by changing their size color etc.).</li>
+        <li>Events: event type attributes allow for the inclusion of JavaScript, by enabling a functionality when the attribute takes effect.</li>
+        <p> Values:<br />
+            Attribute values can be expressed as strings, numbers, URLs, and Booleans. The value will depend on the participating attribute and the usage of it. 
+            Attributes can allow for multiple values to be assign on them, the values will be separated by spaces.
+        </p>
+    </ul>
+    `})
