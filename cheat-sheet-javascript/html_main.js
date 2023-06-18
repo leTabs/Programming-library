e83950e7dcc9f13ec35934931b5fe4cc2de80bba
leTabs <125308756+leTabs@users.noreply.github.com>
2023-06-18 17:06:58 +0300

    Update html_main.js

--- cheat-sheet-javascript/html_main.js
+++ cheat-sheet-javascript/html_main.js
@@ -1,1 +1,17 @@
+const chapters = document.querySelector('.chapters')
 
+const basics = [
+    'Tags',
+    'Elements',
+    'Page Structure',
+    'Atributes'
+]
+
+function list(){
+    chapters.innerHTML+= `<h1>Introduction</h1><hr>`
+    chapters.innerHTML += `<h3>Basics</h3>`
+    for(item in basics){
+        chapters.innerHTML +=  `<p class="chapterNames basic${item}">${basics[item]}</p>`
+    }
+}
+list()
