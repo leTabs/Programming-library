b7efa2c5f2a36a2c0ef437ac62ad95fd657d30ab
leTabs <125308756+leTabs@users.noreply.github.com>
2023-06-18 17:15:38 +0300

    Update html_main.js

--- cheat-sheet-javascript/html_main.js
+++ cheat-sheet-javascript/html_main.js
@@ -47,3 +47,23 @@ basic0.addEventListener('click', ()=>{
     </p>
     `
 })
+
+const basic1 = document.querySelector('.basic1')
+basic1.addEventListener('click', ()=>{
+    context.innerHTML = ` 
+    <h1>Elements</h1>
+    <p>
+        Elements are the components of web pages and are created by using the HTML tags. Elements can contain information, like text images etc. 
+They can also include “Attributes” that provide additional information to the element or alter the element’s behavior.
+    </p>
+    <h2>Inline elements</h2>
+    <p>
+        There are inline elements and block elements.
+Inline elements appear to the website next to other elements in the webpage, covering only as much width as they need so that they fit horizontally.
+    </p>
+    <h2>Block elements</h2>
+    <p>
+        Block elements start on a new line. In addition,
+         the browsers will automatically add some margin (space) around the element.
+    </p>
+    `})
