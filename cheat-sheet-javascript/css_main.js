d444b9db6132c8c246cc3578b02916292bd4dc1a
leTabs <125308756+leTabs@users.noreply.github.com>
2023-06-28 18:34:56 +0300

    Update css_main.js

--- cheat-sheet-javascript/css_main.js
+++ cheat-sheet-javascript/css_main.js
@@ -34,16 +34,30 @@ const library = [
 ]
 function list(){
     chapters.innerHTML+= `<h3>Introduction</h3><hr/>`
-    chapters.innerHTML+= '<p class="html_intro">What is CSS ?</p>'
+    chapters.innerHTML+= '<p class="css_intro">What is CSS ?</p>'
     chapters.innerHTML += `<h3>Basics</h3><hr/>`
     for(item in basics){
         chapters.innerHTML +=  `<p class="chapterNames basic${item}">${basics[item]}</p>`
     }
-    
+    chapters.innerHTML += `<h3>Cummon Properties</h3><hr />`
+    for(item in cummonProperties){
+        chapters.innerHTML += `<p class="chapterName cummonP${item}">${cummonProperties[item]}</p>`
+    }
+    chapters.innerHTML += `<h3>Layout</h3><hr/>`
+    for(item in layout){
+      chapters.innerHTML += `<p class="chapterNames layout${item}">${layout[item]}</p>`
+    }
     chapters.innerHTML += `<h3>Additionaly</h3><hr/>`
     for(item in additionaly){
       chapters.innerHTML += `<p class="chapterNames additionaly${item}">${additionaly[item]}</p>`
     }
-
+    chapters.innerHTML += `<h3>Responsive Web Design</h3><hr/>`
+    for(item in responsiveWD){
+      chapters.innerHTML += `<p class="chapterNames responsiveWD${item}">${responsiveWD[item]}</p>`
+    }
+    chapters.innerHTML += `<h3>Library</h3><hr/>`
+    for(item in library){
+      chapters.innerHTML += `<p class="chapterNames library${item}">${library[item]}</p>`
+    }
 }
 list()
