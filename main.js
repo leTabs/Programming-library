9c85722c033fa3feeba6b8e1464a75bf35880c2d
leTabs <125308756+leTabs@users.noreply.github.com>
2023-06-28 18:16:00 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,22 +1,22 @@
 // ADD COMMENTS DUMMY
-const html = document.querySelector('.html')
-const css = document.querySelector('.css')
-const js = document.querySelector('.js')
-const ts = document.querySelector('.ts')
-const python = document.querySelector('.python')
-const relational = document.querySelector('.relational')
+const html = document.querySelector('.btn-html')
+const css = document.querySelector('.btn-css')
+// const js = document.querySelector('.js')
+// const ts = document.querySelector('.ts')
+const python = document.querySelector('.btn-python')
+// const relational = document.querySelector('.relational')
 
 
 html.addEventListener('click', ()=>{window.location = 'html_main.html'})
 css.addEventListener('click', ()=>{window.location = 'css_main.html'})
-js.addEventListener('click', ()=>{window.location = 'js_main.html'})
-ts.addEventListener('click', ()=>{window.location = 'ts_main.html'})
+// js.addEventListener('click', ()=>{window.location = 'js_main.html'})
+// ts.addEventListener('click', ()=>{window.location = 'ts_main.html'})
 python.addEventListener('click', ()=>{window.location = 'python_main.html'})
-relational.addEventListener('click', ()=>{window.location = 'sql_main.html'})
+// relational.addEventListener('click', ()=>{window.location = 'sql_main.html'})
 
 
-// 
-const relational_btn = document.querySelector('.relational_btn')
-relational_btn.addEventListener('click', ()=>{
-    alert('kiss')
-})
+// // 
+// const relational_btn = document.querySelector('.relational_btn')
+// relational_btn.addEventListener('click', ()=>{
+//     alert('kiss')
+// })
